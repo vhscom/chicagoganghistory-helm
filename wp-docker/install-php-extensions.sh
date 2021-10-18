7b490835756e946728e0324a458d0b6983d0de5f
Maarten de Waard <maarten@greenhost.nl>
2021-10-18 17:18:16 +0200

    copy install-php-extensions commands from current WordPress dockerfile

--- wp-docker/install-php-extensions.sh
+++ wp-docker/install-php-extensions.sh
@@ -2,18 +2,33 @@
 
 # install the PHP extensions (from the WP Dockerfile)
 set -ex;
-
 savedAptMark="$(apt-mark showmanual)";
 
 apt-get update;
 apt-get install -y --no-install-recommends \
+	libfreetype6-dev \
 	libjpeg-dev \
+	libmagickwand-dev \
 	libpng-dev \
-	libzip-dev \
-;
+	libwebp-dev \
+	libzip-dev;
 
-docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr;
-docker-php-ext-install gd mysqli opcache zip;
+docker-php-ext-configure gd \
+	--with-freetype \
+	--with-jpeg \
+	--with-webp \
+;
+docker-php-ext-install -j "$(nproc)" \
+	bcmath \
+	exif \
+	gd \
+	mysqli \
+	zip \
+;
+# https://pecl.php.net/package/imagick
+pecl install imagick-3.5.0;
+docker-php-ext-enable imagick;
+rm -r /tmp/pear;
 
 # reset apt-mark's "manual" list so that "purge --auto-remove" will remove all build dependencies
 apt-mark auto '.*' > /dev/null;
@@ -24,7 +39,7 @@ ldd "$(php -r 'echo ini_get("extension_dir");')"/*.so \
 	| xargs -r dpkg-query -S \
 	| cut -d: -f1 \
 	| sort -u \
-	| xargs -rt apt-mark manual; \
+	| xargs -rt apt-mark manual;
 
 apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false;
 rm -rf /var/lib/apt/lists/*
