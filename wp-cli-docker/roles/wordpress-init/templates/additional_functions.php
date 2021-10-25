7c3e7666620bfa89fdbe732a7f76e679da12e11c
Maarten de Waard <maarten@greenhost.nl>
2021-10-25 14:24:59 +0200

    add smtp configuration, always add "additional_functions" and optionally add smtp and roles settings to it

--- wp-cli-docker/roles/wordpress-init/templates/additional_functions.php
+++ wp-cli-docker/roles/wordpress-init/templates/additional_functions.php
@@ -1,4 +1,5 @@
 <?php
+{% if WP_OPENID_CONNECT_ROLE_MAPPING_ENABLED %}
 add_action('openid-connect-generic-update-user-using-current-claim', function( $user, $user_claim) {
     // Based on some data in the user_claim, modify the user.
     if ( array_key_exists( '{{ WP_OPENID_CONNECT_SETTINGS.role_key }}', $user_claim ) ) {
@@ -10,3 +11,19 @@ add_action('openid-connect-generic-update-user-using-current-claim', function( $
         }
     }
 }, 10, 2);
+{% endif %}
+{% if WP_SMTP_ENABLED %}
+    // SMTP Authentication
+    add_action( 'phpmailer_init', 'send_smtp_email' );
+    function send_smtp_email( $phpmailer ) {
+        $phpmailer->isSMTP();
+        $phpmailer->Host       = SMTP_HOST;
+        $phpmailer->SMTPAuth   = SMTP_AUTH;
+        $phpmailer->Port       = SMTP_PORT;
+        $phpmailer->Username   = SMTP_USER;
+        $phpmailer->Password   = SMTP_PASS;
+        $phpmailer->SMTPSecure = SMTP_SECURE;
+        $phpmailer->From       = SMTP_FROM;
+        $phpmailer->FromName   = SMTP_NAME;
+    }
+{% endif %}
